9d0dfcc0432f657532d68f776950bc160b2532d5
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-08-06 22:10:46 +0900

    preventDefault

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 
 function onClick(){
     console.log(loginInput.value);
-    alert(loginInput.value);
 }
 
-loginButton.addEventListener("click",onClick)
+function onSubmit(fxxk){
+    fxxk.preventDefault();
+    console.log(fxxk);
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit",onSubmit);
+loginButton.addEventListener("click",onClick);

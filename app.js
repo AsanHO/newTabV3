3f6309e17969a81109f9615a12d732424c6e86d1
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-08-06 21:46:49 +0900

    event

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert("hi");
+const title = document.querySelector("div.hello:first-child h1");
+console.log(title);
+
+title.style.color = "blue";
+
+function helloWorld() {
+    console.log("hello world!");
+}
+function changeColorRed() {
+    title.style.color = "red"
+}
+function changeColorBlue() {
+    title.style.color = "blue"
+}
+title.addEventListener("click",helloWorld);
+
+title.addEventListener("mouseenter",changeColorRed);
+title.addEventListener("mouseleave",changeColorBlue);

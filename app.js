69fcaaa4d25e3991cb0d9c7e98a2be972cb92d55
이은호 <eunho@ieunhoui-MacBookAir.local>
2024-08-08 21:50:09 +0900

    refactoring

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const savedUsername = localStorage.getItem(USERNAME);
 function onClick(){
     console.log(loginInput.value);
 }
-
+function showUsername(name){
+    loginForm.classList.add(HIDDEN);
+    welcomeText.classList.remove(HIDDEN);
+    welcomeText.innerText = `환영합니다. ${name}`;
+}
 function onSubmit(event){
     event.preventDefault();
     //이벤트리스너에 함수를 넣었기 때문에 자동으로 생기는 메소드임! 걱정 노노 
@@ -20,16 +24,15 @@ function onSubmit(event){
     // console.log(event);
     // console.log(username);
     localStorage.setItem(USERNAME,username);
-    loginForm.classList.add(HIDDEN);
-    welcomeText.classList.remove(HIDDEN);
-    welcomeText.innerText = `환영합니다. ${username}`;
+    showUsername(username);
 }
 
 loginForm.addEventListener("submit",onSubmit);
 loginButton.addEventListener("click",onClick);
 
+
 if (savedUsername !== null){
     loginForm.classList.add(HIDDEN);
     welcomeText.classList.remove(HIDDEN);
-    welcomeText.innerText = `환영합니다. ${savedUsername}`;
+    showUsername(savedUsername);
 }
